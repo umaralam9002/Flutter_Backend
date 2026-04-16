9e4716f4002706c825f89e9af170add267091bb3
umaralam9002 <umaralam9002@gmail.com>
2026-04-17 01:08:55 +0500

    Folder Structure Created and Connect Firebase

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// routes
+app.use("/api/auth", require("./routes/authRoutes"));
+// app.use("/api/admin", require("./routes/adminRoutes"));
+// app.use("/api/teacher", require("./routes/teacherRoutes"));
+// app.use("/api/student", require("./routes/studentRoutes"));
+
+module.exports = app;
---
+++ src/routes/authRoutes.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+
+const { db } = require("../config/firebase");
+
+router.get("/test", (req, res) => {
+  res.json({
+    message: "Auth route working ",
+  });
+});
+
+// for firebase testing is working or not 
+
+router.get("/firebase-test", async (req, res) => {
+  try {
+    const docRef = db.collection("test").doc("check");
+    
+    await docRef.set({
+      message: "Firebase working 🚀",
+      time: new Date(),
+    });
+
+    const doc = await docRef.get();
+
+    res.json({
+      success: true,
+      data: doc.data(),
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+});
+
+module.exports = router;
