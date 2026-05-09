a7f18fe497a0a3b76625631f8584c32d74cdbc23
umaralam9002 <umaralam9002@gmail.com>
2026-05-09 21:10:53 +0500

    for deployment i change route of firebasekey.json

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,7 @@
 
 const admin = require('firebase-admin');
-const serviceAccount = require('../../firebaseKey.json');
+// const serviceAccount = require('../../firebaseKey.json');
+const serviceAccount = require('/etc/secrets/firebaseKey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
